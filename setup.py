1f4e0b8494b4e29a07384635a70256993fdef971
Rick Sayre <whorfin@gmail.com>
2024-09-04 11:44:11 -0700

    Refactor - cleanup the subprocess launch ...and fix a bug!  non-HLS playback was not properly controlled cache! which led to greatly increased memory use and track drift

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("pypi.md", "r") as fh:
 
 setuptools.setup(
     name="somafm",
-    version="2.6",
+    version="2.7",
     scripts=['somafm'],
     author="Lord Whorfin",
     author_email="whorfin@gmail.com",
