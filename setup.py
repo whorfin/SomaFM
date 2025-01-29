5a8749e383d58e734205c11778c41aa5032d7a68
Rick Sayre <whorfin@gmail.com>
2025-01-28 20:41:38 -0800

    document stream switch keys when active, color for clarity

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pysomafm",
-    version="3.6.0",
+    version="3.6.1",
     scripts=['somafm'],
     author="Lord Whorfin",
     author_email="whorfin+somafm@gmail.com",
